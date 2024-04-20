4ba934249a9a1352a4821c8179c9b9e47cc7804f
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-04-20 13:25:05 +0200

    task 0

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,7 +12,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def app_view_ok():
     """
     app_view_ok method defines the /status route
